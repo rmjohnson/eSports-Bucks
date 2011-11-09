dfdfb0c31ac5ed0178d6c5ce1213ec3920b91a72
Ryan Johnson <kritstov@gmail.com>
2011-11-08 18:53:38 -0600

    Added a view function to the community controller

--- application/classes/controller/community.php
+++ application/classes/controller/community.php
@@ -2,24 +2,34 @@
 
 class Controller_Community extends Controller_ESB {
 
+	//Listing / searching for communities
+	//If a param is included then view a community
 	public function action_index()
 	{
-		//Listing / searching for communities
-		//If a param is included then view a community
+		
 	}
 	
+	//Create a community
 	public function action_add()
 	{
-		//Create a community
+		
 	}
 	
+	//View community information
+	public function action_view()
+	{
+	
+	}
+	
+	//Edit information about a community
 	public function action_edit()
 	{
-		//Edit information about a community
+		
 	}
 	
+	//Permalink to join a community
 	public function action_join()
 	{
-		//Permalink to join a community
+		
 	}
 }
